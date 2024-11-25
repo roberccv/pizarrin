1b1c5741353755b53b3a5283d0ee73f4914bb904
Miguel Gamboa Sánchez <miguel.gamboasanchez@usp.ceu.es>
2024-11-25 23:51:37 +0100

    Sustitución de página default de express por 01-pizarrin.html

--- pizarrin/routes/index.js
+++ pizarrin/routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+var path = require('path');
+
+//INICIALIZACIÓN DEL HTML DE PIZARRÍN [25/11]
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.sendFile(path.join(__dirname, '..', 'public', '01-pizarrin.html')); // Sirve el archivo HTML
 });
 
 module.exports = router;
